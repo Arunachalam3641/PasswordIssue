815b19acc03d8d2800feaecc65b2d14ebcc9c831
Arunachalam13 <karunachalam406@gmail.com>
2024-01-31 11:50:09 +0530

    Added the sample.

---
+++ PasswordIssue/PasswordIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PasswordIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PasswordIssue/PasswordIssue/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace PasswordIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            On<iOS>().SetUseSafeArea(true);
+        }
+    }
+
+}
